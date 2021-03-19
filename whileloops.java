fa515676f73576e55eeab67775420f415a83be57
Avijeet <54774303+avijeet25@users.noreply.github.com>
2021-03-19 12:44:20 +0530

    whileloops

---
+++ whileloops.java
@@ -0,0 +1,15 @@
+public class whileloops {
+    
+
+        public static void main(String[] args) {
+            int number = 23;
+
+            while (number < 34) {
+                System.out.println("Okay" + " " + number);
+
+                number = number + 2;
+            }
+        }
+    
+
+}
